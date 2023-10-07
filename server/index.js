46714ba42918bccba730609b94b3be487f11b208
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-10-07 19:33:17 +0100

    Added POST endpoint for creating a new todo

--- server/index.js
+++ server/index.js
@@ -2,21 +2,33 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
-//middleware 
+//middleware
 app.use(cors());
-app.use(express.json()) 
+app.use(express.json());
 //routes//
 //create todo
 
+app.post("/todos", async (req, res) => {
+  const { description } = req.body;
+  const newTodo = await pool.query(
+    "INSERT INTO todo (description) VALUES($1) RETURNING *",
+    [description]
+  );
+  try {
+    console.log(req.body);
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 //get all todos
+
+
 //get a todo
 //update a todo
 //delete todo
 
-
-
-
 app.listen(5000, () => {
   console.log("server has started on port 500");
 });
-
